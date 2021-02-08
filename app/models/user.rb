9f669883a501d71cbda15bbf7fde31cccbc05240
Matias <matias.acevedoc@gmail.com>
2021-02-08 13:06:56 -0300

    update 08-02

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,9 +27,9 @@ class User < ApplicationRecord
     user
   end
 
-  def welcome_user
-    mailer = MailgunMailer.welcome_email(self)
-    mailer_response = mailer.deliver_now
-    mailgun_message_id = mailer_response.message_id 
+  #def welcome_user
+  #  mailer = MailgunMailer.welcome_email(self)
+  #  mailer_response = mailer.deliver_now
+  #  mailgun_message_id = mailer_response.message_id 
   end
 end
