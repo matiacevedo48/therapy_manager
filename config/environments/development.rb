783a0815f79d7440c65b04916ee864f9d90d066a
Matias <matias.acevedoc@gmail.com>
2021-02-08 16:07:55 -0300

    update 16:07

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'powerful-oasis-02752.herokuapp.com' }
 
   #Configuracion Mailing
   
